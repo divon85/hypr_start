92bcfe85b6579bde6c79ce54dd5af273ce7723e3
divon85 <igornovid@outlook.com>
2024-03-26 23:41:17 +0900

    bug fixing

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ source .install/library.sh
 source .install/required.sh
 source .install/yay.sh
 source .install/packages.sh
+source .install/install-packages.sh
 source .install/displaymanager.sh
 source .install/cleanup.sh
 source .install/reboot.sh
