5accd85adf21f87bf590bf327806f6c49bfd444e
divon85 <igornovid@outlook.com>
2024-12-15 14:29:29 +0900

    update all

--- install.sh
+++ install.sh
@@ -1,83 +1,88 @@
-#!/usr/bin/env sh
+#!/bin/bash
+#  _   _                    ____  _             _             
+# | | | |_   _ _ __  _ __  / ___|| |_ __ _ _ __| |_ ___ _ __  
+# | |_| | | | | '_ \| '__| \___ \| __/ _` | '__| __/ _ \ '__| 
+# |  _  | |_| | |_) | |     ___) | || (_| | |  | ||  __/ |    
+# |_| |_|\__, | .__/|_|    |____/ \__\__,_|_|   \__\___|_|    
+#        |___/|_|                                             
+#  
 
 # ----------------------------------------------------- 
 # Version
 # ----------------------------------------------------- 
-version=$(cat .install/version)
+version=$(cat install/version)
 
 # ----------------------------------------------------- 
 # Library
 # ----------------------------------------------------- 
-source .install/includes/library.sh
+source install/includes/library.sh
 
 # ----------------------------------------------------- 
 # Header
 # ----------------------------------------------------- 
-source .install/includes/header.sh
+source install/includes/header.sh
 
 # ----------------------------------------------------- 
 # Check supported Linux distribution
 # ----------------------------------------------------- 
-source .install/includes/checkdistro.sh
-
-
+source install/includes/checkdistro.sh
 
 # ----------------------------------------------------- 
 # Load Library for used distro
 # ----------------------------------------------------- 
-source .install/$installer/library.sh
+source install/$installer/library.sh
 
 # ----------------------------------------------------- 
 # Install required packages
 # ----------------------------------------------------- 
-source .install/$installer/install_required.sh
+source install/$installer/install_required.sh
 
 # ----------------------------------------------------- 
 # Confirm Start
 # ----------------------------------------------------- 
-source .install/includes/confirm.sh
+source install/includes/confirm.sh
 
 # ----------------------------------------------------- 
 # Preparation
 # ----------------------------------------------------- 
-source .install/includes/preparation.sh
+source install/includes/preparation.sh
 
 # ----------------------------------------------------- 
 # Install Hyprland Packages
 # ----------------------------------------------------- 
-source .install/$installer/install_packages.sh
+source install/$installer/install_packages.sh
 
 # ----------------------------------------------------- 
 # Create Backup
 # ----------------------------------------------------- 
-source .install/includes/backup.sh
+source install/includes/backup.sh
 
 # ----------------------------------------------------- 
 # Restore settings
 # ----------------------------------------------------- 
-source .install/includes/restore.sh
+source install/includes/restore.sh
 
 # ----------------------------------------------------- 
 # Keyboard
 # ----------------------------------------------------- 
-source .install/includes/keyboard.sh
+source install/includes/keyboard.sh
 
 # ----------------------------------------------------- 
 # Screen Resolution
 # ----------------------------------------------------- 
-source .install/includes/monitor.sh
+source install/includes/monitor.sh
 
 # ----------------------------------------------------- 
 # KVM
 # ----------------------------------------------------- 
-source .install/includes/kvm.sh
+source install/includes/kvm.sh
 
 # ----------------------------------------------------- 
 # Copy the configuration
 # ----------------------------------------------------- 
-source .install/includes/copy.sh
+source install/includes/copy.sh
 
 # ----------------------------------------------------- 
 # Reboot
 # ----------------------------------------------------- 
-source .install/includes/reboot.sh
+source install/includes/reboot.sh
