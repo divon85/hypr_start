8cb96f1916fdb0a98080a9235e3cd16618824198
divon85 <igornovid@outlook.com>
2025-06-13 11:26:30 +0900

    Fresh restart

--- install/includes/preparation.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d ~/.dotfiles ] ;then
-    mkdir ~/.dotfiles
-fi
-if [ -d ~/.dotfiles/$version ] ;then
-    rm -rf ~/.dotfiles/$version
-fi
-cp -r dotfiles ~/.dotfiles/$version
-echo ":: ML4W Hyprland Starter configuration prepared in ~/.dotfiles/$version"
